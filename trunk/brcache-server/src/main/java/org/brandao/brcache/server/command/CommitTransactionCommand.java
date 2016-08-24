19a6faecaefa2d0f7772b33a94b6f0652772925c
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-08-24 14:46:47 +0000

    #

---
+++ trunk/brcache-server/src/main/java/org/brandao/brcache/server/command/CommitTransactionCommand.java
@@ -0,0 +1,29 @@
+package org.brandao.brcache.server.command;
+
+import org.brandao.brcache.Cache;
+import org.brandao.brcache.TXCache;
+import org.brandao.brcache.server.Terminal;
+import org.brandao.brcache.server.TerminalReader;
+import org.brandao.brcache.server.TerminalWriter;
+import org.brandao.brcache.server.error.ServerErrorException;
+import org.brandao.brcache.server.error.ServerErrors;
+import org.brandao.brcache.tx.CacheTransaction;
+
+public class CommitTransactionCommand 
+	extends AbstractCommand{
+
+	public void executeCommand(Terminal terminal, Cache cache, TerminalReader reader,
+			TerminalWriter writer, String[] parameters)
+			throws Throwable {
+
+		if(!(cache instanceof TXCache)){
+			throw new ServerErrorException(ServerErrors.ERROR_1009);
+		}
+		
+		TXCache txCahe = (TXCache)cache;
+		CacheTransaction tx = txCahe.getTransactionManager().getCurrrent();
+		tx.commit();
+		
+	}
+
+}
