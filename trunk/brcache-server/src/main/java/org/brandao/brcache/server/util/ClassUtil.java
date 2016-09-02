f5d7331300a8e02f077848dbaf48dac36d0783e6
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-09-02 16:13:12 +0000

    #

---
+++ trunk/brcache-server/src/main/java/org/brandao/brcache/server/util/ClassUtil.java
@@ -0,0 +1,16 @@
+package org.brandao.brcache.server.util;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class ClassUtil {
+
+    public static Class<?> getClasse( String name ) throws ClassNotFoundException{
+        return Class.forName( name, true, Thread.currentThread().getContextClassLoader() );
+    }
+	
+	public static Object toObject(Class<?> type, String value) 
+			throws IllegalAccessException, IllegalArgumentException, 
+			InvocationTargetException, NoSuchMethodException, SecurityException{
+		return type.getMethod("valueOf", Object.class).invoke(type, value);
+	}
+}
