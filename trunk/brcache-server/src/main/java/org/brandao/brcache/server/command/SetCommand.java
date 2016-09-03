d6b7d98374ef811ee292612cd9fe9de1ab5e72d0
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-09-03 22:31:18 +0000

    #

---
+++ trunk/brcache-server/src/main/java/org/brandao/brcache/server/command/SetCommand.java
@@ -0,0 +1,124 @@
+package org.brandao.brcache.server.command;
+
+import java.io.InputStream;
+
+import org.brandao.brcache.BasicCache;
+import org.brandao.brcache.CacheErrors;
+import org.brandao.brcache.CacheException;
+import org.brandao.brcache.server.Terminal;
+import org.brandao.brcache.server.TerminalConstants;
+import org.brandao.brcache.server.TerminalReader;
+import org.brandao.brcache.server.TerminalWriter;
+import org.brandao.brcache.server.error.ServerErrorException;
+import org.brandao.brcache.server.error.ServerErrors;
+
+/**
+ * Representa o comando <code>replace</code>.
+ * Sua sintaxe é:
+ * <pre>
+ * set &lt;key&gt; &lt;timeToLive&gt; &lt;timeToIdle&gt; &lt;size&gt; &lt;reserved&gt;\r\n
+ * &lt;data&gt;\r\n
+ * end\r\n 
+ * </pre> 
+ * @author Brandao
+ *
+ */
+public class SetCommand extends AbstractCommand{
+
+	public void executeCommand(Terminal terminal, BasicCache cache, TerminalReader reader,
+			TerminalWriter writer, String[] parameters)
+			throws Throwable {
+		
+        int timeToLive;
+        int timeToIdle;
+        int size;
+		String key;
+
+		try{
+			key = parameters[1];
+			
+			if(key == null){
+		        throw new NullPointerException();
+			}
+	    }
+	    catch(Throwable e){
+	        throw new ServerErrorException(ServerErrors.ERROR_1003, "key");
+	    }
+		
+        try{
+        	timeToLive = Integer.parseInt(parameters[2]);
+        	if(timeToLive < 0){
+        		throw new IllegalStateException();
+        	}
+        }
+        catch(Throwable e){
+            throw new ServerErrorException(ServerErrors.ERROR_1003, "timeToLive");
+        }
+
+        try{
+        	timeToIdle = Integer.parseInt(parameters[3]);
+        	if(timeToIdle < 0){
+        		throw new IllegalStateException();
+        	}
+        }
+        catch(Throwable e){
+            throw new ServerErrorException(ServerErrors.ERROR_1003, "timeToIdle");
+        }
+
+        try{
+            size = Integer.parseInt(parameters[4]);
+        	if(size <= 0){
+        		throw new IllegalStateException();
+        	}
+        }
+        catch(Throwable e){
+            throw new ServerErrorException(ServerErrors.ERROR_1003, "size");
+        }
+        
+        InputStream stream = reader.getStream(size);
+        InputStream result = null;
+        Throwable error    = null;
+        
+        try{
+            result = cache.putIfAbsentStream(
+                key, 
+                stream,
+                timeToLive,
+                timeToIdle);
+        }
+        catch(Throwable e){
+        	//capturado erro no processamento do fluxo de bytes do item
+        	error = e;
+        }
+        finally{
+            if(stream != null){
+            	try{
+                	//tenta fechar o fluxo
+            		stream.close();
+            	}
+            	catch(Throwable e){
+            		//se error for null, a falha a ser considerada é do 
+            		//fechamento do fluxo.
+            		if(error == null){
+            			throw new ServerErrorException(e, ServerErrors.ERROR_1004);
+            		}
+            	}
+            }
+            
+            //Lança o erro encontrado no processamento da stream.
+            if(error != null){
+            	//Se for lançada a exceção CacheException com o erro ERROR_1030, significa que
+            	//o replace foi concluido, mas não foi possível obter o stream anterior porque ele
+            	//expirou no momento em que se tentou obtê-lo.
+            	if(!(error instanceof CacheException) || ((CacheException)error).getError() != CacheErrors.ERROR_1030){
+            		throw new ServerErrorException(error, ServerErrors.ERROR_1004);
+            	}
+            }
+        }
+        
+    	writer.sendMessage(result != null || error != null? TerminalConstants.REPLACE_SUCCESS : TerminalConstants.NOT_STORED);
+        writer.flush();
+        
+	}
+
+}
