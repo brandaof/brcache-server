b688fe26f7a723a8f9856ef969621f1b81b6890d
Afonso Brandao <brandaof@users.noreply.github.com>
2016-09-25 18:56:28 +0000

    #

--- src/main/java/org/brandao/brcache/server/Terminal.java
+++ src/main/java/org/brandao/brcache/server/Terminal.java
@@ -100,7 +100,7 @@ public class Terminal {
             this.cache           = cache;
             this.readBufferSize  = readBufferSize;
             this.writeBufferSize = writeBufferSize;
-            this.reader          = new TextTerminalReader(this.socket, cache.getConfig().getMemory(), streamFactory, readBufferSize);
+            this.reader          = new TextTerminalReader(this.socket, streamFactory, readBufferSize);
             this.writer          = new TextTerminalWriter(this.socket, streamFactory, writeBufferSize);
             this.run             = true;
             this.terminalVars    = terminalVars;
